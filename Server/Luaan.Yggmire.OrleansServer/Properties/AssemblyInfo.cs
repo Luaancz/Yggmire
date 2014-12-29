70bec90351772a3cbc531c3dbb3131b909b28826
Luaancz <Luaancz@users.noreply.github.com>
2014-12-29 17:25:17 +0100

    Added informational versions.

---
+++ Server/Luaan.Yggmire.OrleansServer/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿[assembly: System.Reflection.AssemblyInformationalVersion("D")]
+[assembly: System.Reflection.AssemblyVersion("0.1.0")]
+[assembly: System.Reflection.AssemblyFileVersion("0.1.0")]
+[assembly: System.Reflection.AssemblyTitle("Yggmire Server Grains")]
+[assembly: System.Reflection.AssemblyProduct("Yggmire Engine")]
+[assembly: System.Reflection.AssemblyCopyright("Copyright © Luaan 2014")]
+[assembly: System.Reflection.AssemblyCompany("Jiří Borkovec (Luaan)")]
+[assembly: System.Runtime.InteropServices.ComVisible(false)]
+
+
